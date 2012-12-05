39ed08d50ed35bfe78980050330c00069a082eb3
Nick DiRienzo <me@nickdirienzo.com>
2012-12-05 09:43:37 -0500

    Still stuck. :/

--- conway3d.py
+++ conway3d.py
@@ -72,7 +72,7 @@ box_size = 10
 padding = 20
 mid_x = (((GRID_SIZE - 1) * (padding - box_size)) + (GRID_SIZE * box_size)) / 2
 mid_y = (((GRID_SIZE - 1) * (padding - box_size)) + (GRID_SIZE * box_size)) / 2
-mid_z = ((((GRID_SIZE - 1) * -1 * (padding - box_size)) + (GRID_SIZE * box_size))) / 2
+mid_z = -100 + (-1 * ((((GRID_SIZE - 1) * (padding - box_size)) + (GRID_SIZE * box_size)))) / 2
 for x in range(GRID_SIZE):
     for y in range(GRID_SIZE):
         for z in range(GRID_SIZE):
@@ -96,12 +96,13 @@ def on_draw():
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
     glPushMatrix()
-    glTranslatef(-mid_x, -mid_y, -50)
-    #glRotatef(45, 0, 1, 0)
+    glTranslatef(mid_x, mid_y, -50) # Why is this backwards
+    #glRotatef(20, 1, 0, 0)
     for x in range(GRID_SIZE):
         for y in range(GRID_SIZE):
             for z in range(GRID_SIZE):
                 grid[x][y][z].draw()
+    glColor3f(1, 0, 0)
     glBegin(GL_LINES)
     glVertex3f(mid_x, -100, 0)
     glVertex3f(mid_x, 100, 0)
@@ -112,15 +113,16 @@ def on_draw():
     glEnd()
     glBegin(GL_LINES)
     glVertex3f(-100, -100, mid_z)
-    glVertex3f(100, 100, mid_z)
+    glVertex3f(200, 200, mid_z)
     glEnd()
     glPopMatrix()
 
 def update(t):
-    global fovy
+    global fovy, rotate
     if keys[pyglet.window.key.UP]:
         fovy -= 1
     if keys[pyglet.window.key.DOWN]:
         fovy += 1
+
 pyglet.clock.schedule_interval(update,1/20.0)
 pyglet.app.run()
