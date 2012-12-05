b3b616d8138d5201508cec3d4e37dcf53f406356
Nick DiRienzo <me@nickdirienzo.com>
2012-12-05 12:47:10 -0500

    Added README and changed color palette.

--- conway3d.py
+++ conway3d.py
@@ -12,6 +12,15 @@ fovy = 60.0
 GRID_SIZE = 5
 grid = [[[0] * GRID_SIZE for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
 
+def RGB_to_GL(r, g, b):
+    return (r/255.0, g/255.0, b/255.0)
+
+COLORS = (RGB_to_GL(85,98,112),
+          RGB_to_GL(78,205,196),
+          RGB_to_GL(199,244,100),
+          RGB_to_GL(255,107,107),
+          RGB_to_GL(196,77,88)) # http://www.colourlovers.com/palette/1930/cheer_up_emo_kid
+
 class Box(object):
     def __init__(self, x, y, z, size, color, render_type):
         self.position = euclid.Vector3(x, y, z)
@@ -76,13 +85,15 @@ for x in range(GRID_SIZE):
             xc = (x * (box_size + padding))
             yc = (y * (box_size + padding))
             zc = (-1 * z * (box_size + padding)) - 100
-            grid[x][y][z] = Box(xc, yc, zc, box_size, (random.random(), random.random(), 1), GL_QUADS)
+            color = random.sample(COLORS, 1)[0]
+            grid[x][y][z] = Box(xc, yc, zc, box_size, color, GL_QUADS)
 
 mid_x = float((grid[-1][0][0].position[0] + box_size) - grid[0][0][0].position[0]) / 2
 mid_y = float((grid[0][-1][0].position[1] + box_size) - grid[0][0][0].position[1]) / 2
 mid_z = float((grid[0][0][-1].position[2] - box_size) + grid[0][0][0].position[2]) / 2
 print mid_x, mid_y, mid_z
 ang = 0
+position = [0, 0, -200]
 
 @window.event
 def on_draw():
@@ -98,10 +109,10 @@ def on_draw():
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
     glPushMatrix()
-    glTranslatef(0, 0, -200)
-    glRotatef(ang, 1, 0, 0)
-    glRotatef(ang, 0, 1, 0)
-    glTranslatef(-mid_x, -mid_y, -mid_z)
+    glTranslatef(*position) # 4th
+    glRotatef(ang, 1, 0, 0) # 3rd
+    glRotatef(ang, 0, 1, 0) # 2nd
+    glTranslatef(-mid_x, -mid_y, -mid_z) # 1st
     for x in range(GRID_SIZE):
         for y in range(GRID_SIZE):
             for z in range(GRID_SIZE):
@@ -110,11 +121,11 @@ def on_draw():
     ang += 5
 
 def update(t):
-    global fovy, rotate
+    global position
     if keys[pyglet.window.key.UP]:
-        fovy -= 1
+        position[2] += 5
     if keys[pyglet.window.key.DOWN]:
-        fovy += 1
+        position[2] -= 5
 
 pyglet.clock.schedule_interval(update,1/20.0)
 pyglet.app.run()
